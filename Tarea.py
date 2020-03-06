d3978834cc808a1a9717b3ae1cdfdf1a3cdf1a64
Andre-villa <Andre-villa@users.noreply.github.com>
2020-03-05 19:51:15 -0600

    Delete Tarea.py

--- Tarea.py
+++
@@ -1,8 +0,0 @@
-def convertir_decimal(pNum,pBase, pExpo=0):
-    if pNum < pBase :
-        return pNum*pBase**pExpo
-    else:
-        return (pNum%10*pBase**pExpo + convertir_decimal(pNum//10,pBase, pExpo+1))
-print(convertir_decimal(315,7))
-
-#Andrey Villanueva carnet:2020097733
