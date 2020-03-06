ab82727c3120369b88188ab124e96fc8c710e944
Andre-villa <Andre-villa@users.noreply.github.com>
2020-03-05 19:46:15 -0600

    Commit #1
    Cambia de cualquier base a base 10

---
+++ Tarea Taller de programacion-pasar de base(2-9) a base(10).py
@@ -0,0 +1,6 @@
+def convertir_decimal(pNum,pBase, pExpo=0):
+    if pNum < pBase :
+        return pNum*pBase**pExpo
+    else:
+        return (pNum%10*pBase**pExpo + convertir_decimal(pNum//10,pBase, pExpo+1))
+print(convertir_decimal(10010100,2))
