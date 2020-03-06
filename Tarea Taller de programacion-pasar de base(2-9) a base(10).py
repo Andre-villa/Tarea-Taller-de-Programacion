58d556036f639b71f7f034339d44bb91c90e1b91
Andre-villa <Andre-villa@users.noreply.github.com>
2020-03-05 20:00:03 -0600

    Commit#3

--- Tarea Taller de programacion-pasar de base(2-9) a base(10).py
+++ Tarea Taller de programacion-pasar de base(2-9) a base(10).py
@@ -3,4 +3,5 @@ def convertir_decimal(pNum,pBase, pExpo=0):
         return pNum*pBase**pExpo
     else:
         return (pNum%10*pBase**pExpo + convertir_decimal(pNum//10,pBase, pExpo+1))
-print(convertir_decimal(100101001,2))
+
+print(convertir_decimal(100010101001,2))
